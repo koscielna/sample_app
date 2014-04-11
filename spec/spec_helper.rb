4822a82213344ed1c0f0f9889ffdb8ed6389a965
Aleksandra Nabożny <aleksandra.nabozny@gmail.com>
2014-04-11 22:39:29 +0200

    Specs added.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,11 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+# require 'capybara'
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
