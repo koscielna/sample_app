457d73b6653c3cd7f771147bbe5d83e5879be9e6
Aleksandra Nabożny <aleksandra.nabozny@gmail.com>
2014-09-11 21:05:25 +0200

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -21,6 +21,11 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
   end
 
   describe "About page" do
@@ -28,6 +33,11 @@ describe "Static pages" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
     end
   end
 end
