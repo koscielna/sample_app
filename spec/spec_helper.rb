4080a075f2a79ed318fe97040a2523713cfb594d
Aleksandra Nabożny <aleksandra.nabozny@gmail.com>
2014-09-13 00:35:14 +0200

    Finish chapter 5

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,6 +41,8 @@ Spork.prefork do
     # order dependency and want to debug it, you can fix the order by providing
     # the seed, which is printed after each run.
     #     --seed 1234
+    config.include Rails.application.routes.url_helpers
+
     config.order = "random"
     config.include Capybara::DSL
   end
